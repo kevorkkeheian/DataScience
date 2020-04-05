fda29420eaa5ee0183d0edf42e8e78d68fef979e
kevorkkeheian <kevork.keheian@codgram.com>
2020-04-05 13:53:16 +0300

    Updated the script

--- scripts/covid19_daily.py
+++ scripts/covid19_daily.py
@@ -11,16 +11,22 @@ CasesByCountry_Yesterday = CasesByCountry[-1]
 
 
 CasesByCountry_Now = CasesByCountry_Now.fillna(0)
+CasesByCountry_Yesterday = CasesByCountry_Yesterday.fillna(0)
 
-#Not necessay to remove the "+"" signs, but if needed the below is the code to do so
-#CasesByCountry_Now['NewCases'] = CasesByCountry_Now['NewCases'].str.replace('+', '')
-#CasesByCountry_Now['NewDeaths'] = CasesByCountry_Now['NewDeaths'].str.replace('+', '')
+if CasesByCountry_Now['NewCases'].dtypes == object:
+    CasesByCountry_Now['NewCases'] = CasesByCountry_Now['NewCases'].str.replace(',', '')
 
-CasesByCountry_Now['NewCases'] = CasesByCountry_Now['NewCases'].str.replace(',', '')
-CasesByCountry_Now['NewDeaths'] = CasesByCountry_Now['NewDeaths'].str.replace(',', '')
+if CasesByCountry_Now["NewDeaths"].dtypes == object:
+        CasesByCountry_Now['NewDeaths'] = CasesByCountry_Now['NewDeaths'].str.replace(',', '')
 
-CasesByCountry_Yesterday['NewCases'] = CasesByCountry_Yesterday['NewCases'].str.replace(',', '')
-CasesByCountry_Yesterday['NewDeaths'] = CasesByCountry_Yesterday['NewDeaths'].str.replace(',', '')
+
+if CasesByCountry_Yesterday['NewCases'].dtypes == object:
+    CasesByCountry_Yesterday['NewCases'] = CasesByCountry_Yesterday['NewCases'].str.replace(',', '')
+
+if CasesByCountry_Yesterday['NewDeaths'].dtypes == object:
+    CasesByCountry_Yesterday['NewDeaths'] = CasesByCountry_Yesterday['NewDeaths'].str.replace(',', '')
+
+
 
 CasesByCountry_Now["NewCases"] = CasesByCountry_Now["NewCases"].astype(float)
 CasesByCountry_Now["NewDeaths"] = CasesByCountry_Now["NewDeaths"].astype(float)
@@ -30,6 +36,10 @@ CasesByCountry_Yesterday["NewDeaths"] = CasesByCountry_Yesterday["NewDeaths"].as
 
 
 CasesByCountry_Now = CasesByCountry_Now.fillna(0)
+CasesByCountry_Yesterday = CasesByCountry_Yesterday.fillna(0)
+
+
+
 
 CasesByCountry_Now.to_csv('./datasets/worldmeter_now.csv', sep = ",")
 CasesByCountry_Yesterday.to_csv('./datasets/worldmeter_yesterday.csv', sep = ",")
