54fec991040ba771855264667a3d1b6e48e27094
kevorkkeheian <kevork.keheian@codgram.com>
2020-11-06 12:57:19 +0200

    ✨ [Add] create `population`

---
+++ scripts/population.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import world_bank_data as wb
+
+population = wb.get_series('SP.POP.TOTL', id_or_value='id', simplify_index=True).reset_index()
+
+
+population = population.loc[(population['SP.POP.TOTL'] > 0)]
+population['SP.POP.TOTL'].fillna(0, inplace=True)
+
+population = population.loc[(population.index > 11600)]
+
+population["SP.POP.TOTL"] = population["SP.POP.TOTL"].astype(int)
+population["Year"] = population["Year"].astype(int)
+population = population.loc[(population['SP.POP.TOTL'] > 0)]
+
+population.to_csv("./datasets/world-bank/population.csv")
